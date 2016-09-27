1edf06e52b90ebeec3d9b025441d70e0c6c23f85
dmarchena <dmarchena@users.noreply.github.com>
2016-09-27 20:11:35 +0200

    RGB 0-255 test passed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
-var postcss = require('postcss');
+import postcss from 'postcss';
+import parser from 'postcss-value-parser';
+
+function hasTransparency(colorFn) {
+	return colorFn.nodes.reduce((acc, curr) => {
+		return acc || (curr.type === 'div' && curr.value === '/');
+	}, false);
+}
+
+function transformSlash(node) {
+	if (node.type !== 'div' || node.value !== '/') {
+		return false;
+	}
+	node.value = ',';
+	node.before = node.after = '';
+	return true;
+}
+
+function transformSpace(node) {
+	if (node.type !== 'space') {
+		return false;
+	}
+	node.type = 'div';
+	node.value = ',';
+	return true;
+}
+
+function parseFnToAlternateSyntax(colorFn) {
+	if (hasTransparency(colorFn)) {
+		colorFn.value = 'rgba';
+		return parser.walk(colorFn.nodes, node => {
+			transformSpace(node) || transformSlash(node);
+		});
+	} else {
+		return parser.walk(colorFn.nodes, transformSpace);
+	}
+}
+
+function transformRgb(value) {
+	return parser(value).walk(node => {
+		if (node.type !== 'function' || (node.value !== 'rgb' && node.value !== 'rgba')) {
+			return;
+		}
+		node = parseFnToAlternateSyntax(node);
+	}).toString();
+}
 
 module.exports = postcss.plugin('postcss-color-rgb', function (opts) {
     opts = opts || {};
@@ -6,8 +51,13 @@ module.exports = postcss.plugin('postcss-color-rgb', function (opts) {
     // Work with options here
 
     return function (root, result) {
+        root.walkDecls(decl => {
+			if (!decl.value || (decl.value.indexOf('rgb(') === -1 && decl.value.indexOf('rgba(') === -1)) {
+				return;
+			}
 
-        // Transform CSS AST here
+			decl.value = transformValue(decl.value);
 
+        });
     };
 });
